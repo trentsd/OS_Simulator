e774b0854fe7c291cf57ae13301cb549bd47952b
Sean McDermott <mcdermottsj@vcu.edu>
2017-11-21 08:57:15 -0500

    Added clock. Can be tested with EXE command.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        BlockingQueue queue = new LinkedBlockingQueue();
+        CpuClock clock = new CpuClock(queue);
+        new Thread(clock).start();
+
+        CommandLine cli = new CommandLine(queue);
+
+
     }
 }
