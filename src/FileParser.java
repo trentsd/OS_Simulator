7021e9acad7d8d23f291b8c37a4dc758347cb5ea
James Conklin <jcconklin@gmail.com>
2017-11-30 13:36:44 -0500

    some delicious delicious code

--- src/FileParser.java
+++ src/FileParser.java
@@ -2,12 +2,20 @@ import java.io.*;
 import java.util.Scanner;
 
 public class FileParser {
-    public static void parse(String filename){
+
+    public void parse(String filename){
         File f = new File(filename);
 
         try (BufferedReader r = new BufferedReader(new FileReader(f))) {
             String line = r.readLine();
             String type = selectType(line);
+
+            if(type.equals("PROGRAM")){
+                parseProgram(line, r);
+            }
+            else if(type.equals("JOB")){
+                parseJob(line, r);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -19,16 +27,49 @@ public class FileParser {
         Scanner in = new Scanner(line);
 
         if(in.hasNextInt()){
+            in.close();
             return "PROGRAM";
         }
-        else return "JOB";
+        else {
+            in.close();
+            return "JOB";
+        }
     }
 
-    private static void parseJob(){
-
+    private static void parseJob(String firstLine, BufferedReader r){
+        Main.cli.interpretInput(firstLine);
+        String line;
+        try {
+            while ((line = r.readLine()) != null){
+                Main.cli.interpretInput(line);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
-    private static void parseProgram(){
+    private static void parseProgram(String line, BufferedReader r){
+        Scanner in = new Scanner(line);
+        int memReq = in.nextInt();
+        try {
+            while ((line = r.readLine()) != null) {
+                in = new Scanner(line);
 
+                String commandToken = in.next();
+
+                switch(commandToken.toUpperCase()){
+                    case "CALCULATE":
+                        break;
+                    case "YIELD":
+                        break;
+                    case "I/O":
+                        break;
+                    case "OUT":
+                        
+                }
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
